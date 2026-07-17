fc04bda884df1d26bd0810203933938294149a2e
Chetan Dhapkas <chetandhapkas27@gmail.com>
2026-07-17 21:29:48 +0530

    frontend folder with updation

--- backend/src/main/java/com/nurture/backend/controller/UserController.java
+++ backend/src/main/java/com/nurture/backend/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.nurture.backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend Connected Successfully";
+    }
 }
