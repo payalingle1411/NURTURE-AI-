6d0c610d400f68c87b2c942a6ed0c35d0adeacd6
Abhinav Gulhane <abhinavgulhane4@gmail.com>
2026-08-20 00:41:45 +0530

    Update the .env file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://10.157.64.217:8080/api",
+  baseURL: "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
